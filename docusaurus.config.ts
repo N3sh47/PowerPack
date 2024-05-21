ef1d38d9944a7b875e4de3cd912a6beab43a3ee7
Nesh47 <munenejohn@gmail.com>
2024-05-21 20:58:00 +0300

    code fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: '',
+  title: 'PowerPack',
   tagline: 'Product Documentation',
   favicon: 'img/favicon.ico',
 
