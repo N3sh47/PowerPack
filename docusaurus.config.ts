2a89ddf3f6703146dfabac7919ee1cb14e66c7ba
Nesh47 <munenejohn@gmail.com>
2024-05-28 22:58:24 +0300

    code replacement

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,6 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-          routeBasePath: '/',
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false, // Disable the blog plugin
